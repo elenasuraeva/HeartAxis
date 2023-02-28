ee4f10bb6d5011467dabf365e61a361101265f01
olgOk <olga.ok@yahoo.com>
2023-02-27 19:17:36 -0800

    general change in test-train procedures

--- main_for_lin_regr.py
+++ main_for_lin_regr.py
@@ -5,88 +5,59 @@ import sklearn.datasets as dt
 from linear.annealing_lin_regression import QALinearRegression
 from utils import get_data_for_training
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from sklearn.metrics import r2_score
 
 if __name__ == "__main__":
     
-    # small test on artificial data. Change the precision vector before running.
-
-    # # define a random dataset for a qa linear regression to test
-    # seed = 11
-    # rand_state = 11
-    # rand= np.random.RandomState(seed)   
-    # x_points, y_labels = dt.make_regression(n_samples=10, n_features=1, noise=1, random_state=rand_state)
-
-    # # define a qa linear regression
-    # qa_lin_model = QALinearRegression()
-    # qa_lin_model.train(x_points, y_labels)
-    # y_predicted = qa_lin_model.predict(x_points)
-    # print("y_predicted = ", y_predicted)
-
-    # model = LinearRegression()
-    # trained_model = model.fit(x_points, y_labels)
-    # y_predict_classical = trained_model.predict(x_points)
-    # # # plot and save file
-    # plt.scatter(x_points, y_labels)
-    # plt.plot(x_points, y_predict_classical, color='pink', linewidth=3, label='Classical')
-    # plt.plot(x_points, y_predicted, color='green', linewidth=1, label='Quantum')
-    # plt.legend(loc='upper left')
-    # plt.title("Adiabatic vs Classical Linear Regression on Artificial Data")
-    # plt.savefig("results/artificial.png")
-    # plt.show()
-
-
+    #TODO: implement train-test split
     age, qrs = get_data_for_training("QRSAxis") #get data
 
+    age_train, age_test, qrs_train, qrs_test = train_test_split(age, qrs, test_size=0.25, random_state=42) 
     # define and train an adiabatic Linear regresson.
     print("Initialize and train Quantum Linear Regression . . .")
-    qrs_lin_regression = QALinearRegression()
-
-    qrs_lin_regression.train(age.reshape(-1, 1), qrs)
-
-    qrs_predicted = qrs_lin_regression.predict(age.reshape(-1,1))
-
-    # print('age_test = ', age_test)
-    # print("qrs_predicted by quantum = ", qrs_predicted)
+    precision_vector = np.array([0, 0.125, 0.25, 0.5, 0.75])
+    qrs_lin_regression = QALinearRegression(precision_vector=precision_vector, normalize=True, scaler="RobustScaler")
+    qrs_lin_regression.train(age_train.reshape(-1, 1), qrs_train)
+    qrs_predicted = qrs_lin_regression.predict(age_train.reshape(-1,1))
 
     # compare to a classical linear regression in Scikit-learn
     print("Initialize and train Classical Linear Regression . . .")
     model= LinearRegression()
-    trained_model = model.fit(age.reshape(-1, 1), qrs)
-    y_predict = trained_model.predict(age.reshape(-1,1))
+    trained_model = model.fit(age_train.reshape(-1, 1), qrs_train)
+    y_predict = trained_model.predict(age_train.reshape(-1,1))
 
-    print("R^2 for Scikit-Learn:", r2_score(qrs, y_predict))
+    print("R^2 on training dataset for Scikit-Learn:", r2_score(qrs_train, y_predict))
+    print("R^2 on training dataset for Adiabatic Linear Regression:", qrs_lin_regression.r_score(qrs_train, qrs_predicted))
     # plot and save file
-    plt.scatter(age, qrs)
-    plt.plot(age, y_predict, color='pink', linewidth=3, label='Classical')
-    plt.plot(age, qrs_predicted, color='green', linewidth=1, label='Quantum')
+    plt.scatter(age_train, qrs_train)
+    plt.plot(age_train, y_predict, color='pink', linewidth=2, label='Classical')
+    plt.plot(age_train, qrs_predicted, color='green', linewidth=3, label='Quantum')
     plt.legend(loc='upper left')
     plt.title("QRS Axis (in degrees) Depending on Age")
-    # plt.savefig("results/quantum.png")
-    # plt.show()
+    plt.savefig("results/sklearn_vs_adiabatic_train.png")
+    plt.show()
 
-    # model verification.
+    # model evaluation.
     print("Evaluate Quantum Linear Regression . . .")
-    age_verif = np.array([58, 76, 79, 57, 52, 56, 73, 50, 33, 68, 70, 53])
-    qrs_axis_actual = np.array([-20, 34, 42, 45, 30, 16, 12, 30, 35, 53, 55, 2])
+    qrs_predicted_eval = qrs_lin_regression.predict(age_test.reshape(-1,1))
+    qrs_classical = model.predict(age_test.reshape(-1,1))
 
-    qrs_predicted2 = qrs_lin_regression.predict(age_verif.reshape(-1,1))
-
-    print("Age {}".format(age_verif))
-    print("Predicted QRS Axis {}".format(qrs_predicted))
+    print("Age {}".format(age_test))
+    print("Predicted QRS Axis {}".format(qrs_predicted_eval))
 
     # print metrics
-    print('MAE = ', qrs_lin_regression.mae(qrs, qrs_predicted))
-    print('MSE = ', qrs_lin_regression.mse(qrs, qrs_predicted))
-    print('Root MSE = ', qrs_lin_regression.root_mse(qrs, qrs_predicted))
-    print('R^2 = ', qrs_lin_regression.r_score(qrs, qrs_predicted))
+    print('MAE for the Adiabatic Linear Regression = ', qrs_lin_regression.mae(qrs_test, qrs_predicted_eval))
+    print('MSE for the Adiabatic Linear Regression = ', qrs_lin_regression.mse(qrs_test, qrs_predicted_eval))
+    print('Root MSE for the Adiabatic Linear Regression = ', qrs_lin_regression.root_mse(qrs_test, qrs_predicted_eval))
+    print('R^2 on test dataset for the Adiabatic Linear Regression = ', qrs_lin_regression.r_score(qrs_test, qrs_predicted_eval))
+    print('R^2 on test dataset for the Sklearn Linear Regression = ', qrs_lin_regression.r_score(qrs_test, qrs_classical))
 
-    # # plot and save file
-    # plt.scatter(age_verif, qrs_axis_actual)
-    # # plt.plot(age_verif, qrs_axis_actual, color='pink', linewidth=3, label='Actual')
-    # plt.plot(age_verif, qrs_predicted, color='green', linewidth=1, label='Quantum Predicted')
-    # plt.legend(loc='upper left')
-    # plt.title("QRS Axis (in degrees) Depending on Age")
-    # plt.savefig("results/actualquantum.png")
-    # plt.show()
+    plt.scatter(age_test, qrs_test)
+    plt.plot(age_test, qrs_classical, color='pink', linewidth=2, label='Classical')
+    plt.plot(age_test, qrs_predicted_eval, color='green', linewidth=3, label='Quantum')
+    plt.legend(loc='upper left')
+    plt.title("QRS Axis (in degrees) Depending on Age")
+    plt.savefig("results/sklearn_vs_adiabatic_test.png")
+    plt.show()
